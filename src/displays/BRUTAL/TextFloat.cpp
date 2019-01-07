c6cf1253079884d13f0c6b43bd84541ffd9db41d
Connor G Meehan <connorgmeehan23@gmail.com>
2019-01-08 03:51:34 +1100

    Added Text float channel

---
+++ src/displays/BRUTAL/TextFloat.cpp
@@ -0,0 +1,37 @@
+#include "TextFloat.h"
+
+TextFloat::TextFloat(std::string fontPath, std::string message, int fontSize) {
+    mFont.load(fontPath, fontSize, true, true, true);
+
+    istringstream in(message);
+    copy(istream_iterator<string>(in), istream_iterator<string>(), back_inserter(mMessage));
+    mTotalWidth = mFont.stringWidth(message);
+}
+
+void TextFloat::setup(){
+
+}
+
+void TextFloat::update(DrawModel & model, StateModel & state){
+
+}
+
+void TextFloat::draw(DrawModel & model, StateModel & state){
+    float offset = -mTotalWidth/2;
+    for(auto & message : mMessage) {
+        mFont.drawStringAsShapes(newMessage, offset, 0);
+        offset += mFont.stringWidth(newMessage);
+    }
+}
+
+void TextFloat::onKick(float amp, float vel){
+
+}
+
+void TextFloat::onSnare(float amp, float vel){
+
+}
+
+void TextFloat::onHat(float amp, float vel){
+
+}
---
+++ src/displays/BRUTAL/TextFloat.h
@@ -0,0 +1,25 @@
+#ifndef __TextFloat_H__
+#define __TextFloat_H__
+
+#include "ofMain.h"
+#include "Channel.h"
+#include "DrawModel.h"
+
+class TextFloat : public Channel {
+    public:
+        TextFloat(std::string fontPath, std::string message, int fontSize);
+        std::string getName(){ return std::string("TextFloatChannel"); }
+        void setup();
+        void update(DrawModel & model, StateModel & state);
+        void draw(DrawModel & model, StateModel & state);
+
+        void onKick(float amp, float vel);
+        void onSnare(float amp, float vel);
+        void onHat(float amp, float vel);
+    private:
+        std::vector<std::string> mMessage;
+        ofTrueTypeFont mFont;
+        float mTotalWidth;
+};
+
+#endif
